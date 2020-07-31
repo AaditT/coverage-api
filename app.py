6160d54d0189cffc0b973b8eb764531a94aabec5
AaditT <aadit.trivedi03@gmail.com>
2020-07-31 10:54:21 -0700

    reduced time even more

--- app.py
+++ app.py
@@ -216,7 +216,7 @@ def v2(lat, lon):
         index = queries.index(query)
         googlenews.search(query)
         results = googlenews.result()
-        for i in range(3):
+        for i in range(1):
             urls[index].append(results[i]['link'])
             extractive_scores[index].append(lavaa.lavaa_extractive(query, results[i]['link']))
             abstractive_scores[index].append(lavaa.lavaa_abstractive(query, results[i]['link']))
