b5777cfc28c0bf9cbe6eb02e3f87e5b3a0d1f5ec
vedu mallela <vedu.mallela@gmail.com>
2020-07-31 11:35:24 -0400

    international location added

--- resp.py
+++ resp.py
@@ -1,12 +1,16 @@
 import requests
+import reverse_geocoder as rg
 
 def get_location_data(lat, longitude):
-    lat= str(lat)
-    longitude = str(longitude)
+    lat_str= str(lat)
+    long_str = str(longitude)
 
-    fips_data = requests.get("https://geo.fcc.gov/api/census/area?lat=" + lat + "&lon=" + longitude + "&format=json")
+    fips_data = requests.get("https://geo.fcc.gov/api/census/area?lat=" + lat_str + "&lon=" + long_str + "&format=json")
     fips_json = fips_data.json()
-    # print(fips_json)
-
+    if(len(fips_json['results']) == 0):
+        coordinates = (lat, longitude)
+        results = rg.search(coordinates,mode=1)
+        return results[0]['name'], results[0]['cc']
 
     return fips_json.get('results')[0].get('county_name')
+print(get_location_data(51.5074, -0.1278))
