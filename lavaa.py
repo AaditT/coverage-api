175245ce88aedbbb6847ec8e2be6398aaac36690
AaditT <aadit.trivedi03@gmail.com>
2020-07-30 18:42:17 -0700

    changed requirements

--- lavaa.py
+++ lavaa.py
@@ -1,14 +1,12 @@
 from newspaper import fulltext
 import requests
-from nltk.corpus import stopwords
 
-stop_words = set(stopwords.words('english'))
 
 def lavaa_extractive(query, url):
     lavaa = 0
     text = (fulltext(requests.get(url).text)).replace("\n", " ")
     query_words = query.split()
-    text_words = [word for word in text.split() if not word in stop_words]
+    text_words = text.split()
     text_words_len = len(text_words)
     for word in text_words:
         if word in query_words:
