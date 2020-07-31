08ec28de3f0055ed3368d05b959952d2ade7a026
AaditT <aadit.trivedi03@gmail.com>
2020-07-31 00:06:04 -0700

    routing between pages

--- app.py
+++ app.py
@@ -13,96 +13,8 @@ def home():
 
     <h2> Welcome!
 
-    <h4> API v1 </h4>
-    <code> /api/v1/lat/lon </code>
-      <p>
-        Version 1 is optimized for speed. <br>
-        Returns JSON object in following format <br>
-        <code>
-          { <br>
-           &nbsp; "lat": lat, <br>
-          &nbsp; "lon": lon, <br>
-          &nbsp; "time": { <br>
-           &nbsp; &nbsp; "timestamp": now, <br>
-            &nbsp; }, <br>
-          &nbsp; "data": { <br>
-           &nbsp; &nbsp; "policies": { <br>
-          &nbsp; &nbsp; &nbsp; "urls": [url1, url2, url3, etc], <br>
-          &nbsp; &nbsp; }, <br>
-          &nbsp; &nbsp; "education": { <br>
-          &nbsp; &nbsp; &nbsp; "urls": [url1, url2, url3, etc], <br>
-          &nbsp; &nbsp; }, <br>
-          &nbsp; &nbsp; "biology": { <br>
-          &nbsp; &nbsp; &nbsp; "urls": [url1, url2, url3, etc], <br>
-          &nbsp; &nbsp; }, <br>
-          &nbsp; &nbsp; "economy": { <br>
-          &nbsp; &nbsp; &nbsp; "urls": [url1, url2, url3, etc], <br>
-          &nbsp; &nbsp; }, <br>
-          &nbsp; &nbsp; "statistics": { <br>
-          &nbsp; &nbsp; &nbsp; "urls": [url1, url2, url3, etc], <br>
-          &nbsp; &nbsp; }, <br>
-            &nbsp; }, <br>
-            }
-        </code>
-      </p>
-
-      <h4> API v2 </h4>
-      <code> /api/v2/lat/lon </code>
-      <p>
-        Version 2 is optimized for querying metrics. <br>
-        Returns JSON object in following format <br>
-        <code>
-          { <br>
-          &nbsp; "success": True, <br>
-           &nbsp; "lat": lat, <br>
-          &nbsp; "lon": lon, <br>
-          &nbsp; "time": { <br>
-           &nbsp; &nbsp; "timestamp": now, <br>
-            &nbsp; }, <br>
-          &nbsp; "data": { <br>
-           &nbsp; &nbsp; "policies": { <br>
-          &nbsp; &nbsp; &nbsp; "urls": [url1, url2, url3, etc], <br>
-          &nbsp; &nbsp; &nbsp; "scores": { <br>
-            &nbsp;  &nbsp; &nbsp; &nbsp; "extractive": [score1, score2, score3, etc], <br>
-          &nbsp;  &nbsp; &nbsp; &nbsp; "abstractive": [score1, score2, score3, etc], <br>
-          &nbsp; &nbsp; &nbsp; } <br>
-          &nbsp; &nbsp; }, <br>
-          &nbsp; &nbsp; "education": { <br>
-          &nbsp; &nbsp; &nbsp; "urls": [url1, url2, url3, etc], <br>
-          &nbsp; &nbsp; &nbsp; "scores": { <br>
-            &nbsp;  &nbsp; &nbsp; &nbsp; "extractive": [score1, score2, score3, etc], <br>
-          &nbsp;  &nbsp; &nbsp; &nbsp; "abstractive": [score1, score2, score3, etc], <br>
-          &nbsp; &nbsp; &nbsp; } <br>
-          &nbsp; &nbsp; }, <br>
-          &nbsp; &nbsp; "biology": { <br>
-          &nbsp; &nbsp; &nbsp; "urls": [url1, url2, url3, etc], <br>
-          &nbsp; &nbsp; &nbsp; "scores": { <br>
-            &nbsp;  &nbsp; &nbsp; &nbsp; "extractive": [score1, score2, score3, etc], <br>
-          &nbsp;  &nbsp; &nbsp; &nbsp; "abstractive": [score1, score2, score3, etc], <br>
-          &nbsp; &nbsp; &nbsp; } <br>
-          &nbsp; &nbsp; }, <br>
-          &nbsp; &nbsp; "economy": { <br>
-          &nbsp; &nbsp; &nbsp; "urls": [url1, url2, url3, etc], <br>
-          &nbsp; &nbsp; &nbsp; "scores": { <br>
-            &nbsp;  &nbsp; &nbsp; &nbsp; "extractive": [score1, score2, score3, etc], <br>
-          &nbsp;  &nbsp; &nbsp; &nbsp; "abstractive": [score1, score2, score3, etc], <br>
-          &nbsp; &nbsp; &nbsp; } <br>
-           &nbsp; &nbsp; &nbsp; "scores": { <br>
-            &nbsp;  &nbsp; &nbsp; &nbsp; "extractive": [score1, score2, score3, etc], <br>
-          &nbsp;  &nbsp; &nbsp; &nbsp; "abstractive": [score1, score2, score3, etc], <br>
-          &nbsp; &nbsp; &nbsp; } <br>
-          &nbsp; &nbsp; }, <br>
-          &nbsp; &nbsp; "statistics": { <br>
-          &nbsp; &nbsp; &nbsp; "urls": [url1, url2, url3, etc], <br>
-          &nbsp; &nbsp; &nbsp; "scores": { <br>
-            &nbsp;  &nbsp; &nbsp; &nbsp; "extractive": [score1, score2, score3, etc], <br>
-          &nbsp;  &nbsp; &nbsp; &nbsp; "abstractive": [score1, score2, score3, etc], <br>
-          &nbsp; &nbsp; &nbsp; } <br>
-          &nbsp; &nbsp; }, <br>
-            &nbsp; }, <br>
-            }
-        </code>
-      </p>
+    <h3><a href="/docs">Read the API documentation</a></h3>
+    <iframe src="https://giphy.com/embed/lXiRLb0xFzmreM8k8" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/vector-newspaper-flat-lXiRLb0xFzmreM8k8">via GIPHY</a></p>
 
     """
 
@@ -110,9 +22,10 @@ def home():
 @app.route('/docs')
 def docs():
     return """
+    <h3><a href="/"> <- Home </a></h3>
     <h1> COVerage RESTful API </h1>
 
-    <h2> Welcome!
+    <h2> Welcome to the docs!
 
     <h4> API v1 </h4>
     <code> /api/v1/lat/lon </code>
